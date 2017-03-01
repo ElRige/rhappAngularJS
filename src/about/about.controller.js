e8276b771e0636a82c73f5002f81e75a86b842c7
ElRige <pierre.conord@gmail.com>
2017-03-01 09:17:56 +0100

    Routage

---
+++ src/about/about.controller.js
@@ -0,0 +1,8 @@
+(function () {
+    const app = angular.module('rhapp.about');
+
+    app.controller('AboutController', function () {
+        let vm = this;
+        vm.greetings = 'Welcome to about page';
+    });
+})();
--- src/app.controller.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    const app = angular.module('rhapp');
-
-    app.controller('RhappController',  function (rhappService) {
-        let vm = this;
-        vm.applications = rhappService.getApplications();
-    });
-})();
